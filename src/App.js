475c2566fb5fa90b2d134d3dfd9c24ce93db7fb7
Tyler Porter <hello@cleverladder.com>
2017-06-30 09:24:08 -0700

    feat: Add ListBooks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ListBooks from './ListBooks'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!
+        <ListBooks />
       </div>
     );
   }
---
+++ src/ListBooks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class ListBooks extends React.Component {
+
+	render(){
+		return(
+
+				<h1>Hello World!</h1>
+
+			)
+
+
+	}
+
+}
+
+export default ListBooks
