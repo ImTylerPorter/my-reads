615c996985b32fd119360a73ce112e599f152262
Tyler Porter <hello@cleverladder.com>
2017-06-30 12:14:31 -0700

    feat: Set up and connect API

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
+
+
 class ListBooks extends React.Component {
+
 
 	render(){
 
@@ -9,16 +12,16 @@ class ListBooks extends React.Component {
 		return(
 
           <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading / Want to read / Read</h2>
+            <h2 className="bookshelf-title">All Books</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
                 
               	{books.map(book => (
 
-	                <li key={book.title}>
+	                <li key={book.id}>
 	                  <div className="book">
 	                    <div className="book-top">
-	                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: ` url("${book.book_cover}")` }}></div>
+	                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: ` url("${book.imageLinks.smallThumbnail}")` }}></div>
 	                      <div className="book-shelf-changer">
 	                        <select>
 	                          <option value="none" disabled>Move to...</option>
@@ -30,7 +33,7 @@ class ListBooks extends React.Component {
 	                      </div>
 	                    </div>
 	                    <div className="book-title">{book.title}</div>
-	                    <div className="book-authors">{book.author}</div>
+	                    <div className="book-authors">{book.authors}</div>
 	                  </div>
 	                </li>
                 ))}
