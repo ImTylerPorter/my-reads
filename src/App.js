c3ae758beb2e66f8ae9ac13c1e30d77315cd61b3
Tyler Porter <hello@cleverladder.com>
2017-07-01 11:13:39 -0700

    feat: Add SearchAndAdd component, Routed to a 'search' page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import ListBooks from "./components/ListBooks";
+import SearchAndAdd from "./components/SearchAndAdd";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={ListBooks} />
+        <Route path="/search" component={SearchAndAdd} />
       </div>
     );
   }
---
+++ src/components/SearchAndAdd.js
@@ -0,0 +1,14 @@
+import React, { Component }from 'react';
+
+class SearchAndAdd extends React.Component {
+
+	render() {
+		return(
+			<h1>HELLO SEARCH PAGE!</h1>
+		)
+	}
+
+
+}
+
+export default SearchAndAdd
