d37a92208d3842590a32053e0f5a9f6b55c89628
Tyler Porter <hello@cleverladder.com>
2017-07-01 11:25:42 -0700

    style: Add basic structure to search page

--- src/components/SearchAndAdd.js
+++ src/components/SearchAndAdd.js
@@ -1,10 +1,43 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import SingleBook from './SingleBook'
 
 class SearchAndAdd extends React.Component {
+	state = {
+		books: []
+	};
+
 
 	render() {
+		const { books } = this.state;
+
 		return(
-			<h1>HELLO SEARCH PAGE!</h1>
+			<div className="search-books">
+	          <div className="search-books-bar">
+	            <Link
+	              to='/'
+	              className='close-search'
+	            >
+	              Close
+	            </Link>
+	            <div className="search-books-input-wrapper">
+	              <input
+	                  type="text"
+	                  placeholder="Search by title or author"
+	              />
+	            </div>
+	          </div>
+	          <div className="search-books-results">
+	            <ol className="books-grid">
+	                {books.map((book, index) =>
+						<SingleBook
+							key={index}
+							book={book}
+						/>
+					)}
+	            </ol>
+	          </div>
+        </div>
 		)
 	}
 
